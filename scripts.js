1a4bf6f4e5a57f2f9dc6972f5cedd1dc3f7b8f3e
T3aC0d3 <85062086+T3aC0d3@users.noreply.github.com>
2024-06-19 20:41:18 +0200

    feat(game): handle user input and generate random computer value
    - Updated JS to capture user input
    - Added random value generation for computer choice

--- scripts.js
+++ scripts.js
@@ -1,14 +1,38 @@
-function randomNumberGenerator() {
-    return Math.floor(Math.random() * 3);
+function getComputerChoice() {
+
+    let number = Math.floor(Math.random() * 3);
+
+    if (number == 0) {
+        console.log("Computer: Rock!")
+    }
+
+    else if (number == 1) {
+        console.log("Computer: Paper!")
+    }
+
+    else {
+        console.log("Computer: Scissors!")
+    }
 }
 
-function realComputerDecision(number) {
-    const decisions = ["Rock!", "Paper!", "Scissors!"];
-    return decisions[number];
+function getHumanChoice() {
+    let choice = prompt("Paper, Rock or Scissors?")
+
+    if (choice.toLocaleLowerCase() === "rock") {
+        console.log("Human: Rock!")
+    }
+
+    else if (choice.toLocaleLowerCase() === "paper") {
+        console.log("Human: Paper!")
+    }
+
+    else if (choice.toLocaleLowerCase() === "scissors") {
+        console.log("Human: Scissors!")
+    }
+
+    else {
+        console.log("Invalid choice!")
+    }
 }
-
-let number = randomNumberGenerator();
-console.log(number);
-
-let decision = realComputerDecision(number);
-console.log(decision);
+getHumanChoice()
+console.log(getComputerChoice());
