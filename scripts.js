22d57419cf769671464fa0c661315e719842426b
T3aC0d3 <85062086+T3aC0d3@users.noreply.github.com>
2024-06-20 19:20:12 +0200

    feat(game): update computer logic, enhance playRound(), add rounds, and implement playGame()
    - Updated computer random value logic
    - Improved playRound() function
    - Added round tracking
    - Implemented playGame() function

--- scripts.js
+++ scripts.js
@@ -1,38 +1,74 @@
 function getComputerChoice() {
-
     let number = Math.floor(Math.random() * 3);
 
-    if (number == 0) {
-        console.log("Computer: Rock!")
-    }
-
-    else if (number == 1) {
-        console.log("Computer: Paper!")
-    }
-
-    else {
-        console.log("Computer: Scissors!")
+    if (number === 0) {
+        console.log("C0mput3rs race: rock")
+        return "rock";
+    } else if (number === 1) {
+        console.log("C0mput3rs race: paper")
+        return "paper";
+    } else if (number === 2) {
+        console.log("C0mput3rs race: scissors")
+        return "scissors";
     }
 }
 
 function getHumanChoice() {
-    let choice = prompt("Paper, Rock or Scissors?")
+    let choice = prompt("Paper, Rock or Scissors?");
+    choice = choice.toLowerCase();
 
-    if (choice.toLocaleLowerCase() === "rock") {
-        console.log("Human: Rock!")
+    if (choice === "rock") {
+        console.log("Human race: rock")
+        return "rock";
+    } else if (choice === "paper") {
+        console.log("Human race: paper")
+        return "paper";
+    } else if (choice === "scissors") {
+        console.log("Human race: scissors")
+        return "scissors";
+    }
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === "rock" && computerChoice === "scissors") {
+        console.log("Human wins! " + humanChoice + " beats " + computerChoice + "\nPoint to human race");
+        humanScore++;
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+        console.log("Human wins! " + humanChoice + " beats " + computerChoice + "\nPoint to human race");
+        humanScore++;
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+        console.log("Human wins! " + humanChoice + " beats " + computerChoice + "\nPoint to human race");
+        humanScore++;
+    } else if (humanChoice == computerChoice) {
+        console.log("Draw! " + "\nNo points were awarded");
+    } else {
+        console.log("C0mput3rs race wins! " + computerChoice + " beats " + humanChoice + "\nPoint to c0mput3rs race");
+        computerScore++;
+    }
+    console.log(humanScore, computerScore)
+}
+
+function playGame() {
+    for (let i = 0; i < 5; i++) {
+        let humanSelection = getHumanChoice();
+        let computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
     }
 
-    else if (choice.toLocaleLowerCase() === "paper") {
-        console.log("Human: Paper!")
+    if (humanScore > computerScore) {
+        console.log("Human race wins tournament")
     }
 
-    else if (choice.toLocaleLowerCase() === "scissors") {
-        console.log("Human: Scissors!")
+    else if (humanScore === computerScore) {
+        console.log("DRAW!!!")
     }
 
     else {
-        console.log("Invalid choice!")
+        console.log("C0mput3rs race wins tournament")
     }
 }
-getHumanChoice()
-console.log(getComputerChoice());
+
+playGame();
